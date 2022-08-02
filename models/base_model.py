b89c204e3a1268bea78d6b625464788952537a0e
Opeyemi Akande <akande212@gmail.com>
2022-08-02 15:10:06 +0100

    Opeyemi's first commit

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,12 @@
+#!/usr/bin/python3
+"""
+    This module defines the BaseModel class and is a
+    class from which all other classes would inherit
+"""
 
+class BaseModel:
+    """
+        The base class for all other classes created 
+        in this project.
+    """
+    pass
