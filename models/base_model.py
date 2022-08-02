c77d08ef8e228792357858e5777752415f769355
Opeyemi Akande <akande212@gmail.com>
2022-08-02 17:30:32 +0100

    Initial commits on BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -9,4 +9,54 @@ class BaseModel:
         The base class for all other classes created 
         in this project..
     """
-    pass
+    from uuid import uuid4
+    from datetime import datetime
+
+
+    @property
+    def id(self):
+        """
+        Get/set the unique id for each instance
+        """
+        return (self.id)
+
+    @id.setter
+    def id(self):
+        """Creates a unique id for each instance"""
+        self.id = uuid4()
+
+    @property
+    def created_at(self):
+        """
+        Get/set datetime when and instance was created
+        """
+        return (self.created_at)
+
+    @created_at.setter
+    def created_at(self):
+        """
+        Create and assign datetime of creation
+        """
+        pass
+
+    @property
+    def updated_at(self):
+        """
+        Get/set datetime when and instance was updated
+        """
+        pass
+
+    def __str__(self):
+        """Define the print() representation of the BaseModel"""
+        pass
+
+    def save(self):
+        """Updates the attribute 'update_at' with the current datetime"""
+        pass
+
+    def to_dict(self):
+        """Returns a dictionary containing key/value pairs of __dict__"""
+        pass
+
+
+
