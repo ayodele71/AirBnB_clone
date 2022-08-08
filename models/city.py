982b5d90aa19419f54e31d77d240fee78d044fa5
Opeyemi Akande <akande212@gmail.com>
2022-08-08 04:43:17 +0100

    some changes

--- models/city.py
+++ models/city.py
@@ -7,8 +7,10 @@ class City(BaseModel):
     """Represent a city.
 
     Attributes:
+
         state_id (str): The state id.
         name (str): The name of the city.
+
     """
 
     state_id = ""
